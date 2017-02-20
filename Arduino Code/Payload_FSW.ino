77b325d791f77ce7d404f0c8b22fd81404a1746b
Bhaskar <bhaskarroberts@gmail.com>
2017-02-19 20:38:15 -0500

    FSW2
    Now generates packets every .5s.

--- Arduino Code/Payload_FSW.ino
+++ Arduino Code/Payload_FSW.ino
@@ -1,14 +1,15 @@
+#include <Wire.h>
 #include <math.h>
 #include <Adafruit_Sensor.h>
-#include <Wire.h>
 #include <Adafruit_HMC5883_U.h> // Magnetometer
 #include <DHT.h> // Thermometer
 #include <DHT_U.h>
 #include <SFE_BMP180.h> // Barometer/Altimeter
+#include <string.h>
 
 // Defined Constants
-#define PI 3.1415926536
 #define TEMPPIN 2 // The digital pin on the Arduino that the thermometer connects to
+#define PITOTPIN A0 // pin for the pitot tube
 #define DECLINATION 12.5 // Princeton, NJ (degrees)
 // #define DECLINATION 4.2167 // Stephenville, Tx (degrees)
 
@@ -18,21 +19,29 @@ DHT_Unified dht(TEMPPIN,DHT22);
 SFE_BMP180 bmp;
 double baseline; // baseline pressure
 int offset;
+int t_pwr_up; // time of power-up
+int pkt_cnt = 0;
+String pkt;
+int state;
+int pic_cnt; // number of pictures taken
 
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(9600);
+  t_pwr_up = rlt(); 
   dht.begin(); // thermometer
   bmp.begin(); // Pressure/Altitude sensor
+  mag.begin(); // mag sensor
   baseline = pressure();  
 
     // Setup for pitot tube (copied from example code)
     int i=0;
     int sum=0;
     offset=0;
+    int sensorValue;
     Serial.println("init...");
     for(i=0;i<10;i++)
     {
-         sensorValue = analogRead(sensorPin)-512;
+         sensorValue = analogRead(PITOTPIN)-512;
          sum+=sensorValue;
     }
     offset=sum/10.0;
@@ -40,21 +49,62 @@ void setup() {
 }
 
 void loop() {
+  Serial.println(packet());
   
+  delay(500);
+}
+
+// Reads sensor data and generates a packet String
+String packet()
+{
+  pkt_cnt++; // increment packet count
   
+  String p = "4234,GLIDER,"; // team_id,GLIDER
+  p += String(rlt() - t_pwr_up) + ","; // MISSION TIME,
+  p += String(pkt_cnt) + ",";
+  int P = pressure(); // read pressure
+  p += String(alt(P)) + ",";
+  p += String(P) + ",";
+  p += String(spd()) + ",";
+  p += String(temp()) + ",";
+  p += String(vltg()) + ",";
+  p += String(heading()) + ",";
+  p += stateToString() + ",";
+  p += String(pic_cnt);
+
+  return p;
+}
+
+// real-time clock reading in seconds
+int rlt()
+{
+  return 0;
+}
+
+// converts int state into a String
+String stateToString()
+{
+    return "STATE";  
+}
+
+// voltage in volts
+double vltg()
+{
+    return 0.0;  
 }
 
 // Sample time, altitude, pressure, speed, temp, voltage, heading, camera
 // in degrees
 double heading() {
   // Step 1: Read magnetometer data
-  sensors_event_t e = mag.getEvent(&e);
+  sensors_event_t e;
+  mag.getEvent(&e);
   double x = e.magnetic.x;
   double y = e.magnetic.y;
   double z = e.magnetic.z;
 
   // Step 2: Calculate the heading
-  double heading = atan2(y, x) * PI/180; // Find heading in degrees
+  double heading = atan2(y, x) * 180/PI; // Find heading in degrees
   heading += DECLINATION;
   
   if (heading < 0)  heading += 360; // Make sure 0 < heading < 360
@@ -65,7 +115,8 @@ double heading() {
 
 // in degrees Celsius
 double temp() {
-  sensors_event_t e = dht.temperature().getEvent(&e);
+  sensors_event_t e;
+  dht.temperature().getEvent(&e);
   return e.temperature;  
 }
 
@@ -75,9 +126,14 @@ double alt(double P) {
 }
 
 // in m/s
-double speed(){
-  
-  
+double spd(){
+  return 0.0;
+  /*double sensorValue = analogRead(PITOTPIN)-offset; 
+  int P=(5*sensorValue)/1024.0 - 2.5;
+  P *= 1000; // in Pa
+
+  double speed = sqrt((2/1.225)*P);
+  return speed;*/
 }
 
 // in mb
@@ -93,7 +149,7 @@ double pressure()
   // If request is successful, the number of ms to wait is returned.
   // If request is unsuccessful, 0 is returned.
 
-  status = pressure.startTemperature();
+  status = bmp.startTemperature();
   if (status != 0)
   {
     // Wait for the measurement to complete:
@@ -105,7 +161,7 @@ double pressure()
     // Use '&T' to provide the address of T to the function.
     // Function returns 1 if successful, 0 if failure.
 
-    status = pressure.getTemperature(T);
+    status = bmp.getTemperature(T);
     if (status != 0)
     {
       // Start a pressure measurement:
@@ -113,7 +169,7 @@ double pressure()
       // If request is successful, the number of ms to wait is returned.
       // If request is unsuccessful, 0 is returned.
 
-      status = pressure.startPressure(3);
+      status = bmp.startPressure(3);
       if (status != 0)
       {
         // Wait for the measurement to complete:
@@ -126,7 +182,7 @@ double pressure()
         // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
         // Function returns 1 if successful, 0 if failure.
 
-        status = pressure.getPressure(P,T);
+        status = bmp.getPressure(P,T);
         if (status != 0)
         {
           return(P);
@@ -138,3 +194,4 @@ double pressure()
     else Serial.println("error retrieving temperature measurement\n");
   }
   else Serial.println("error starting temperature measurement\n");
+}
